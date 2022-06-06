2c2d115563f628c7d6075ff9ba5f8cdc24d07f6a
Mudabbirul Saad <mudabbirulsaad@gmail.com>
2022-06-06 20:39:19 +0600

    cheeseburger: bliss bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cheeseburger.mk
+    $(LOCAL_DIR)/bliss_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cheeseburger-user \
-    aosp_cheeseburger-userdebug \
-    aosp_cheeseburger-eng
+    bliss_cheeseburger-user \
+    bliss_cheeseburger-userdebug \
+    bliss_cheeseburger-eng
