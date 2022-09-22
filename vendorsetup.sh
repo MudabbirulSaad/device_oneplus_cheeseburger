11999ff29815696240184b4ac9caf9b83bb2a729
Mudabbirul Saad <mudabbirulsaad@gmail.com>
2022-09-23 00:20:03 +0600

    vendorsetup.sh: Updated sources.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,31 @@
 # Device Tree
 echo "Cloning OnePlus 5 Tree"
-git clone https://github.com/MudabbirulSaad/device_oneplus_cheeseburger_private -b elixir device/oneplus/cheeseburger
+rm -rf device/oneplus/dumpling
+git clone --depth=1 https://github.com/MudabbirulSaad/device_oneplus_cheeseburger -b elixir device/oneplus/cheeseburger
 echo ""
 
 # Device Tree
 echo "Cloning msm8998-common Tree"
-git clone https://github.com/MudabbirulSaad/device_oneplus_msm8998-common_private -b twelve device/oneplus/msm8998-common
+rm -rf device/oneplus/msm8998-common
+git clone --depth=1 https://github.com/MudabbirulSaad/device_oneplus_msm8998-common -b A13MIX device/oneplus/msm8998-common
 echo ""
 
 echo "Cloning Vendor blobs"
-git clone https://github.com/MudabbirulSaad/vendor_oneplus_dumpcheese_private -b twelve vendor/oneplus
+rm -rf vendor/oneplus
+git clone --depth=1 https://github.com/MudabbirulSaad/vendor_oneplus -b lineage-19.1 vendor/oneplus
 echo ""
 
 echo "Cloning kernel tree"
-git clone https://github.com/MudabbirulSaad/kernel_oneplus_msm8998_private -b twelve kernel/oneplus/msm8998
+rm -rf kernel/oneplus/msm8998
+git clone --depth=1 https://github.com/MudabbirulSaad/kernel_oneplus_msm8998 -b A13 kernel/oneplus/msm8998
 echo ""
 
 echo "Cloning lineage livedisplay"
+rm -rf hardware/lineage/livedisplay
 git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay -b lineage-19.1 hardware/lineage/livedisplay
 echo ""
 
 echo "Cloning oneplus hardware"
-git clone https://github.com/LineageOS/android_hardware_oneplus -b lineage-19.1 hardware/oneplus
+rm -rf hardware/oneplus
+git clone https://github.com/LineageOS/android_hardware_oneplus -b lineage-20 hardware/oneplus
 echo ""
